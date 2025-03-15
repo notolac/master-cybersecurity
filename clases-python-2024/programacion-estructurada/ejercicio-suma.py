027768587b16bac8d2cc37ed3717ef19cc03cc14
notolac <carlos@notolac.org>
2025-03-15 20:45:39 +0000

    add basic arithmetic and string operations examples in new Python files

---
+++ clases-python-2024/programacion-estructurada/ejercicio-suma.py
@@ -0,0 +1,14 @@
+# Declaracion de numeros
+entero = 7
+real = 3.14
+complejo = 5 + 3j
+
+# Operaciones
+
+suma = entero + real
+
+multiplicacion = real * complejo.real
+
+# Imprimir resultados
+print("Suma (entero + real):", suma, "es de tipo", type(suma))
+print("Multiplicacion (real * parte real de complejo):", multiplicacion, "es de tipo", type(multiplicacion))
---
+++ clases-python-2024/programacion-estructurada/ejercicio-suma2.py
@@ -0,0 +1,11 @@
+cadena1 = "Python"
+cadena2 = "es un lenguaje de programación"
+
+# Operación de concatenación
+frase = cadena1 + " " + cadena2
+frase_repetida = frase * 3
+
+# Impresión de la frase
+
+print("Frase concatenada: ", frase)
+print("Frase repetida: ", frase_repetida)
